8607051b7f6138d752619e4d776cd9dfce0802f8
baliharko <bali@fauxdelorean.com>
2022-01-04 10:51:59 +0100

    project setup

---
+++ src/main/kotlin/com/teamjeffry/receptbokenbackend/ReceptbokenBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.teamjeffry.receptbokenbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReceptbokenBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReceptbokenBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamjeffry/receptbokenbackend/ReceptbokenBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamjeffry.receptbokenbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReceptbokenBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
