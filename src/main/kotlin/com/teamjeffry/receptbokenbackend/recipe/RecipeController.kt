cb21ec7432d157a4c293fa0a7ab9a0b056794061
baliharko <bali@fauxdelorean.com>
2022-01-05 10:23:51 +0100

    recipe - getbyname method for solving db issue

--- src/main/kotlin/com/teamjeffry/receptbokenbackend/recipe/RecipeController.kt
+++ src/main/kotlin/com/teamjeffry/receptbokenbackend/recipe/RecipeController.kt
@@ -1,16 +1,20 @@
 package com.teamjeffry.receptbokenbackend.recipe
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("v1/recipe")
 class RecipeController(
     private val recipeRepository: RecipeRepository
 ) {
 
-    @PostMapping("/v1/recipe/save")
+    @PostMapping("/save")
     fun save(@RequestBody request: Recipe): Recipe {
         return recipeRepository.save(request)
+    }
+
+    @GetMapping("/get/{name}")
+    fun getByName(@PathVariable name: String): Recipe {
+        return recipeRepository.findRecipeByName(name);
     }
 }
