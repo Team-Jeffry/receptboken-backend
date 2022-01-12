f49feeb01441f66b2ba3240232f90b01cc97987f
baliharko <bali@fauxdelorean.com>
2022-01-12 11:29:57 +0100

    added service layer to recipe

--- src/main/kotlin/com/teamjeffry/receptbokenbackend/recipe/RecipeController.kt
+++ src/main/kotlin/com/teamjeffry/receptbokenbackend/recipe/RecipeController.kt
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("v1/recipe")
 class RecipeController(
-    private val recipeRepository: RecipeRepository
+    private val recipeService: RecipeService
 ) {
 
     @PostMapping("/save")
     fun save(@RequestBody request: Recipe): Recipe {
-        return recipeRepository.save(request)
+        return recipeService.save(request)
     }
 
     @GetMapping("/get/{name}")
     fun getByName(@PathVariable name: String): Recipe {
-        return recipeRepository.findRecipeByName(name);
+        return recipeService.findRecipeByName(name);
     }
 }
--- src/main/kotlin/com/teamjeffry/receptbokenbackend/recipe/RecipeService.kt
+++ src/main/kotlin/com/teamjeffry/receptbokenbackend/recipe/RecipeService.kt
@@ -1,4 +1,15 @@
 package com.teamjeffry.receptbokenbackend.recipe
 
-class RecipeService {
+class RecipeService(
+    private val recipeRepository: RecipeRepository
+) {
+    fun save(request: Recipe): Recipe {
+        return recipeRepository.save(request);
+    }
+
+    fun findRecipeByName(name: String): Recipe {
+        return recipeRepository.findRecipeByName(name);
+    }
+
+
 }
