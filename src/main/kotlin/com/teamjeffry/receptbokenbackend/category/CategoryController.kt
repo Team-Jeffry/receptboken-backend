c0bb0441786675f0a3aae6a5c98c00d01ea36d1c
Eggdad <simon.nojd.jobb@gmail.com>
2022-01-17 18:16:55 +0100

    Added endpoint to fetch all categories from the DB

---
+++ src/main/kotlin/com/teamjeffry/receptbokenbackend/category/CategoryController.kt
@@ -0,0 +1,20 @@
+package com.teamjeffry.receptbokenbackend.category
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@CrossOrigin("http://localhost:3000/")
+@RequestMapping("/v1/category")
+class CategoryController(
+    private val categoryRepository: CategoryRepository
+) {
+
+    @GetMapping("/all")
+    fun getAllCategories(): ResponseEntity<List<Category>> {
+        return ResponseEntity.ok(categoryRepository.findAll());
+    }
+}
